9acdfc6efb6cbdee7913063d9b8862f7f74bbf33
Nada Stanisich <nstanisich@gmail.com>
2023-10-04 22:38:18 +1100

    Classes

--- models/Character.cs
+++ models/Character.cs
@@ -2,5 +2,5 @@ public class Character
 {
     public string SuperHero { get; set; }
     public string SecretIdentity { get; set; }
-    public Powers SuperPowers { get; set; }
+    public List<SuperPower> SuperPowers { get; set; }
 }
---
+++ models/SuperPower.cs
@@ -0,0 +1,5 @@
+public class SuperPower {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsMagic { get; set; }
+}
