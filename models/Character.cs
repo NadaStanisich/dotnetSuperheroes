7790cea2460a7ea7c8a85101b56a754395d74a4a
Nada Stanisich <nstanisich@gmail.com>
2025-08-14 11:48:04 +1000

    updated to use the more modern target-typed new expression syntax new()

--- models/Character.cs
+++ models/Character.cs
@@ -1,6 +1,8 @@
+namespace dotnetSuperheroes;
+
 public class Character
 {
-    public string SuperHero { get; set; }
-    public string SecretIdentity { get; set; }
-    public List<SuperPower> SuperPowers { get; set; }
+    public required string SuperHero { get; set; }
+    public required string SecretIdentity { get; set; }
+    public required List<SuperPower> SuperPowers { get; set; }
 }
--- models/SuperPower.cs
+++ models/SuperPower.cs
@@ -1,5 +1,8 @@
-public class SuperPower {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public bool IsMagic { get; set; }
+namespace dotnetSuperheroes;
+
+public class SuperPower
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required bool IsMagic { get; set; }
 }
